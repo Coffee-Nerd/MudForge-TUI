63dfe5c5fd185f89da2d4c6b90d73bc3b2e43b84
Jeffrey Johnston <rpgplayers.inc@gmail.com>
2025-02-02 22:23:54 +1100

    Handle parsing for GMCP messages

--- src/telnet_client.rs
+++ src/telnet_client.rs
@@ -380,7 +380,7 @@ fn parse_gmcp(data: &str) -> Option<(String, Value)> {
     None
 }
 
-/// Tries to parse known GMCP modules and return a corresponding TelnetMessage.
+/// Tries to parse known GMCP modules and returns a corresponding TelnetMessage.
 fn parse_known_gmcp_modules(gmcp_str: &str) -> Option<TelnetMessage> {
     if let Some((package, value)) = parse_gmcp(gmcp_str) {
         match package.as_str() {
@@ -391,14 +391,12 @@ fn parse_known_gmcp_modules(gmcp_str: &str) -> Option<TelnetMessage> {
             }
             "char.vitals" => {
                 if let Ok(obj) = serde_json::from_value::<CharVitals>(value) {
-                    return Some(TelnetMessage::CharVitals(
-                        obj.hp,
-                        obj.maxhp,
-                        obj.mana,
-                        obj.maxmana,
-                        obj.moves,
-                        obj.maxmoves,
-                    ));
+                    return Some(TelnetMessage::CharVitals(obj.hp, obj.mana, obj.movement));
+                }
+            }
+            "char.maxstats" => {
+                if let Ok(obj) = serde_json::from_value::<CharMaxStats>(value) {
+                    return Some(TelnetMessage::CharMaxStats(obj.maxhp, obj.maxmana, obj.maxmove));
                 }
             }
             "room.info" => {
@@ -409,12 +407,17 @@ fn parse_known_gmcp_modules(gmcp_str: &str) -> Option<TelnetMessage> {
             "comm.channel" => {
                 if let Ok(cc) = serde_json::from_value::<CommChannel>(value) {
                     let parsed_msg = parse_gmcp_message(&cc.msg);
-                  //  let mut chat_spans = vec![Span::styled(
-                  //      format!("[{}] {}: ", cc.chan, cc.player),
-                  //      Style::default().fg(Color::Green),
-                  //  )];
-                  //  chat_spans.extend(parsed_msg);
                     return Some(TelnetMessage::ChatMessage(parsed_msg));
+                }
+            }
+            "char.status" => {
+                if let Ok(obj) = serde_json::from_value::<CharStatus>(value) {
+                    return Some(TelnetMessage::CharStatus(obj.level, obj.tnl, obj.enemypct));
+                }
+            }
+            "group" => {
+                if let Ok(obj) = serde_json::from_value::<GroupInfo>(value) {
+                    return Some(TelnetMessage::GroupInfo(obj));
                 }
             }
             _ => {}
@@ -423,15 +426,75 @@ fn parse_known_gmcp_modules(gmcp_str: &str) -> Option<TelnetMessage> {
     None
 }
 
-/// Parses inline color markers (like "$R" for red) inside a GMCP message.
+/// Converts a GMCP inline marker (like "$x196" or "$G") into a Color using the ANSI color map.
+fn convert_color_marker(marker: &str) -> Color {
+    if marker.starts_with("$x") {
+        let num_str = &marker[2..];
+        if let Ok(num) = num_str.parse::<u8>() {
+            let key = format!("38;5;{}", num);
+            if let Some(&color) = COLOR_MAP.get(key.as_str()) {
+                return color;
+            }
+        }
+        Color::White
+    } else if marker == "$G" {
+        Color::Rgb(0, 255, 0)
+    } else if marker == "$M" {
+        Color::Rgb(255, 0, 255)
+    } else if marker == "$R" {
+        Color::Rgb(255, 0, 0)
+    } else if marker == "$Y" {
+        Color::Rgb(255, 255, 0)
+    } else if marker == "$B" {
+        Color::Rgb(0, 0, 255)
+    } else if marker == "$C" {
+        Color::Rgb(0, 255, 255)
+    } else if marker == "$w" || marker == "$W" {
+        Color::Rgb(255, 255, 255)
+    } else {
+        Color::White
+    }
+}
+
+/// Parses inline color markers inside a GMCP message.  
+/// This version supports both "$xNNN" markers (using xterm 256‑color lookup)  
+/// as well as single‑character markers like "$G" for green.
 pub fn parse_gmcp_message(msg: &str) -> Vec<Span<'static>> {
     let mut spans = Vec::new();
     let mut current_text = String::new();
     let mut current_color = Color::White;
     let mut chars = msg.chars().peekable();
+
     while let Some(ch) = chars.next() {
         if ch == '$' {
+            // Look ahead to see if we have a valid color marker.
             if let Some(&next_ch) = chars.peek() {
+                if next_ch == 'x' {
+                    // Marker like "$xNNN": consume 'x' and all following digits.
+                    chars.next(); // consume 'x'
+                    let mut num_str = String::new();
+                    while let Some(&digit) = chars.peek() {
+                        if digit.is_ascii_digit() {
+                            num_str.push(digit);
+                            chars.next();
+                        } else {
+                            break;
+                        }
+                    }
+                    let marker = format!("$x{}", num_str);
+                    let new_color = convert_color_marker(&marker);
+                    if !current_text.is_empty() {
+                        spans.push(Span::styled(
+                            current_text.clone(),
+                            Style::default().fg(current_color),
+                        ));
+                        current_text.clear();
+                    }
+                    current_color = new_color;
+                    continue;
+                } else {
+                    // Check for single-character markers.
+                    let marker = format!("${}", next_ch);
                 let new_color = match next_ch {
                     'G' => Some(Color::Rgb(0, 255, 0)),
                     'M' => Some(Color::Rgb(255, 0, 255)),
@@ -444,14 +507,19 @@ pub fn parse_gmcp_message(msg: &str) -> Vec<Span<'static>> {
                 };
                 if let Some(col) = new_color {
                     if !current_text.is_empty() {
-                        spans.push(Span::styled(current_text.clone(), Style::default().fg(current_color)));
+                            spans.push(Span::styled(
+                                current_text.clone(),
+                                Style::default().fg(current_color),
+                            ));
                         current_text.clear();
                     }
-                    chars.next(); // Consume the color code.
+                        chars.next(); // consume the marker letter.
                     current_color = col;
                     continue;
                 } else {
+                        // Unrecognized marker: output the '$' literally.
                     current_text.push(ch);
+                    }
                 }
             } else {
                 current_text.push(ch);
